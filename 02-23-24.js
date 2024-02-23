107d8d50dbf7c91ae15ce169769da7b9d8bf88e0
Peter <Peterinsinga83@gmail.com>
2024-02-23 06:46:57 -0500

    02-23-24 Solution

---
+++ 02-23-24.js
@@ -0,0 +1,26 @@
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+// Examples:
+
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+function longest(s1, s2) {
+    let splitStr = s1 + s2
+    let arr = []
+    let newStr = splitStr.split('')
+  
+  
+        newStr.forEach((b,i)=>{
+      if(arr.includes(b) == false){
+        arr.push(b)
+      }
+    })
+    return arr.sort().join('')
+   
+    
+  
+  }
